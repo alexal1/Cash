070ba6b5e5f6e5d7f78f1f948ccc82588cf6b015
ma <ma@infra.guru>
2019-02-20 14:23:49 +0300

    Add Anko library, customize theme

--- app/src/main/java/com/alex_aladdin/cash/MainActivity.kt
+++ app/src/main/java/com/alex_aladdin/cash/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.alex_aladdin.cash
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintSet.PARENT_ID
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import org.jetbrains.anko.constraint.layout.ConstraintSetBuilder.Side.*
+import org.jetbrains.anko.constraint.layout.applyConstraintSet
+import org.jetbrains.anko.constraint.layout.constraintLayout
+import org.jetbrains.anko.recyclerview.v7.recyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        constraintLayout {
+            val datesRecyclerView = recyclerView {
+                id = View.generateViewId()
+            }.lparams(0, 0)
+
+            applyConstraintSet {
+                connect(
+                    START of datesRecyclerView to START of PARENT_ID,
+                    END of datesRecyclerView to END of PARENT_ID,
+                    TOP of datesRecyclerView to TOP of PARENT_ID,
+                    BOTTOM of datesRecyclerView to BOTTOM of PARENT_ID
+                )
+            }
+        }
     }
 
 }
