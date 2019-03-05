ed66c955571438bdab5516a9a2cc5cfaebfc067d
ma <ma@infra.guru>
2019-03-05 14:41:41 +0300

    Set all dates' time to 00:00:00

--- app/src/main/java/com/alex_aladdin/cash/CashApp.kt
+++ app/src/main/java/com/alex_aladdin/cash/CashApp.kt
@@ -1,12 +1,26 @@
 package com.alex_aladdin.cash
 
 import android.app.Application
+import com.alex_aladdin.cash.utils.currentLocale
 import io.reactivex.subjects.BehaviorSubject
 import java.util.*
+import java.util.Calendar.*
 
 class CashApp : Application() {
 
-    val todayDate = Date()
-    val currentDate = BehaviorSubject.createDefault(todayDate)
+    val todayDate: Date by lazy {
+        GregorianCalendar.getInstance(currentLocale())
+            .apply {
+                set(HOUR, 0)
+                set(MINUTE, 0)
+                set(SECOND, 0)
+                set(MILLISECOND, 0)
+            }
+            .time
+    }
+
+    val currentDate by lazy {
+        BehaviorSubject.createDefault(todayDate)
+    }
 
 }
