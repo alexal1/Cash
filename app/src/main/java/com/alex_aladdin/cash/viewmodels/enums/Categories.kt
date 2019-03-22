cdaf0106ae5c3519c9257e387858a14b0fc0a5e3
ma <ma@infra.guru>
2019-03-22 16:36:14 +0300

    Create full list of LossCategories

--- app/src/main/java/com/alex_aladdin/cash/viewmodels/enums/Categories.kt
+++ app/src/main/java/com/alex_aladdin/cash/viewmodels/enums/Categories.kt
@@ -2,6 +2,7 @@ package com.alex_aladdin.cash.viewmodels.enums
 
 interface Categories {
 
+    val id: String
     val isGain: Boolean
     val colorRes: Int
     val stringRes: Int
--- app/src/main/java/com/alex_aladdin/cash/viewmodels/enums/GainCategories.kt
+++ app/src/main/java/com/alex_aladdin/cash/viewmodels/enums/GainCategories.kt
@@ -5,6 +5,7 @@ import com.alex_aladdin.cash.R
 enum class GainCategories : Categories {
 
     SALARY {
+        override val id: String = name
         override val isGain = true
         override val colorRes = R.color.green
         override val stringRes = R.string.gain_salary
