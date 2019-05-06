3f570e5df0ff93a77d11e0d3900fbe35b261ff78
ma <ma@infra.guru>
2019-05-06 12:54:04 +0300

    Add CategoriesManager and RxJava global error handler to the Application

--- app/src/main/java/com/alex_aladdin/cash/CashApp.kt
+++ app/src/main/java/com/alex_aladdin/cash/CashApp.kt
@@ -2,8 +2,11 @@ package com.alex_aladdin.cash
 
 import android.app.Application
 import android.content.Context
+import android.util.Log.e
+import com.alex_aladdin.cash.helpers.CategoriesManager
 import com.alex_aladdin.cash.helpers.CurrencyManager
 import com.alex_aladdin.cash.utils.currentLocale
+import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.subjects.BehaviorSubject
 import java.util.*
 import java.util.Calendar.*
@@ -13,11 +16,17 @@ class CashApp : Application() {
     companion object {
 
         private const val PREFERENCES_NAME = "CashAppPreferences"
+        private const val TAG = "CashApp"
 
     }
 
+
     val currencyManager by lazy {
         CurrencyManager(sharedPreferences)
+    }
+
+    val categoriesManager by lazy {
+        CategoriesManager(sharedPreferences)
     }
 
     val todayDate: Date by lazy {
@@ -37,6 +46,13 @@ class CashApp : Application() {
 
     private val sharedPreferences by lazy {
         applicationContext.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
+
+
+    init {
+        RxJavaPlugins.setErrorHandler { throwable ->
+            e(TAG, "Uncaught exception: ", throwable)
+        }
     }
 
 }
