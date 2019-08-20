b5851f03b4b1120ddd784ddcad2117139e685aa9
ma <ma@infra.guru>
2019-08-20 14:02:33 +0300

    Add the prefix for all debug logs

--- app/src/main/java/com/madewithlove/daybalance/helpers/timber/CashDebugTree.kt
+++ app/src/main/java/com/madewithlove/daybalance/helpers/timber/CashDebugTree.kt
@@ -9,4 +9,16 @@ import timber.log.Timber
 /**
  * Just logs messages to logcat.
  */
-class CashDebugTree : Timber.DebugTree()
+class CashDebugTree : Timber.DebugTree() {
+
+    companion object {
+        private const val PREFIX = "Cash"
+        private const val MAX_TAG_LENGTH = 23
+    }
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        val tagWithPrefix = "$PREFIX$tag".take(MAX_TAG_LENGTH)
+        super.log(priority, tagWithPrefix, message, t)
+    }
+
+}
