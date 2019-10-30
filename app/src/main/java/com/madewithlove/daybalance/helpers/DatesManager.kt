612fbddacec2635abe090db8252dce428046ff89
ma <ma@infra.guru>
2019-10-30 12:20:53 +0300

    Create DatesManager

---
+++ app/src/main/java/com/madewithlove/daybalance/helpers/DatesManager.kt
@@ -0,0 +1,65 @@
+/**
+ * Created by Alexander Mishchenko in 2019
+ */
+
+package com.madewithlove.daybalance.helpers
+
+import android.text.format.DateUtils
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class DatesManager {
+
+    /**
+     * Time is emitted as GMT+00:00 date with hours, minutes, seconds, milliseconds set to zero.
+     */
+    val currentDateObservable: Observable<Date>
+    val currentDate: Date get() = currentDateSubject.value!!
+
+    /**
+     * Whether current date is today in default time zone.
+     */
+    val isTodayObservable: Observable<Boolean>
+    val isToday: Boolean get() = DateUtils.isToday(currentDate.time)
+
+    private val millisInDay = TimeUnit.DAYS.toMillis(1)
+    private val now = Date(System.currentTimeMillis() / millisInDay * millisInDay)
+    private val currentDateSubject = BehaviorSubject.createDefault(now)
+    private val midnightSubject = PublishSubject.create<Unit>()
+
+
+    init {
+        currentDateObservable = currentDateSubject.distinctUntilChanged()
+
+        isTodayObservable = Observable
+            .merge(
+                currentDateSubject,
+                midnightSubject
+            )
+            .map { isToday }
+            .distinctUntilChanged()
+
+        Timer("DatesManagerThread").schedule(
+            object : TimerTask() {
+                override fun run() {
+                    midnightSubject.onNext(Unit)
+                }
+            },
+            Date(now.time - TimeZone.getDefault().rawOffset + millisInDay),
+            millisInDay
+        )
+    }
+
+
+    fun updateCurrentDate(date: Date) {
+        require(date.time % millisInDay == 0L) {
+            "Only dates in GMT+00:00 with hours, minutes, seconds, milliseconds set to zero are accepted here"
+        }
+
+        currentDateSubject.onNext(date)
+    }
+
+}
